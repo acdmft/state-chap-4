b9d89b7195f1c808e70e730b959a9ed54fcf2e0a
fusbudget <andrewsanser@gmail.com>
2022-01-27 14:59:57 +0100

    add solution to ex-6

--- src/App.js
+++ src/App.js
@@ -14,15 +14,30 @@ const stepsMin = 0;
 const stepsMax = 50000;
 
 class App extends React.Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+    };
+  }
+
+  renderIndicators() {
+    return (
+      <div className="row">
+        <p>Heart: {heartMin}</p>
+        <p>Temperature: {tempMin}</p>
+        <p>Steps: {stepsMin}</p>
+      </div>
+    );
+  }
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <p>Heart: {heartMin}</p>
-          <p>Temperature: {tempMin}</p>
-          <p>Steps: {stepsMin}</p>
-        </div>
+        {this.renderIndicators()}
         <div className="row">
           <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
           <Box icon="directions_walk" color="black" value={3000} unit="steps" />
